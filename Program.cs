5e97ab78c3e202c35fff7307752509e78a50bef7
emrekizilkaya1905 <144436310+emrekizilkaya1905@users.noreply.github.com>
2024-08-23 19:39:09 +0200

    Made Database Connection

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
+using Microsoft.EntityFrameworkCore;
+using MyGalaxy_Data_Access.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,7 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddDbContext<ApplicactionDbContext>(options => { options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")); });
 // First commit emreremerere
 var app = builder.Build();
 
